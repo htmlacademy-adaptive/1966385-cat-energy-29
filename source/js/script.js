3a041fa7549ad34456d976878baeff90c1e3f5f2
Nikita Kostevich <nikitakostevich@gmail.com>
2023-08-16 22:50:11 +0300

    Добавляет адаптивную разметку декоративных элементов

---
+++ source/js/script.js
@@ -0,0 +1,7 @@
+let buttonTog = document.querySelector(".main-nav__toggle");
+let navigation = document.querySelector(".site-list");
+
+buttonTog.onclick = function () {
+  navigation.classList.toggle("site-list--hidden");
+  buttonTog.classList.toggle("main-nav__toggle--open");
+};
