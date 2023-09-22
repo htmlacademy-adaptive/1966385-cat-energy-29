9da5256cb223f70776df337e841a04c4df4cb1a1
Nikita Kostevich <nikitakostevich@gmail.com>
2023-09-22 17:18:59 +0300

    Исправляет замечания после первой проверки

--- source/js/script.js
+++
@@ -1,7 +0,0 @@
-let buttonTog = document.querySelector('.main-nav__toggle');
-let navigation = document.querySelector('.site-list--hidden');
-
-buttonTog.onclick = function () {
-  navigation.classList.toggle('site-list--hidden');
-  buttonTog.classList.toggle('main-nav__toggle');
-};
